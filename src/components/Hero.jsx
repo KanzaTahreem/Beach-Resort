4c4ee3b67e7ad7f80f5065e001f2e0d39bee5842
KanzaTahreem <tehreemmsk411@gmail.com>
2022-12-19 16:50:33 +0500

    Ues default prop to render diff images

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import '../styles/Hero.scss';
 
-const Hero = (props) => {
-  const { children, hero } = props;
+const Hero = (defaultProps) => {
+  const { children, hero } = defaultProps;
 
   return (
     <header className={hero}>
       {children}
     </header>
   );
+};
+
+Hero.defaultProps = {
+  hero: 'defaultHero',
 };
 
 export default Hero;
