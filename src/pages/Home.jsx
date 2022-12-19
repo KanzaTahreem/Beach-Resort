e759972f04fc0847be2768dce699585a99cf64a4
KanzaTahreem <tehreemmsk411@gmail.com>
2022-12-19 16:51:57 +0500

    Setup hero compoment for diff pages

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import Hero from '../components/Hero';
 
 const Home = () => (
-  <div>Hello from home page!</div>
+  <div>
+    <Hero />
+  </div>
 );
 
 export default Home;
