2ff5b51cc64208b360043983c76f13aa8f5f6f8d
KanzaTahreem <tehreemmsk411@gmail.com>
2022-12-31 00:41:33 +0500

    Add banner for diff pages

--- src/pages/Rooms.jsx
+++ src/pages/Rooms.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Hero from '../components/Hero';
+import Banner from '../components/Banner';
 
 const Rooms = () => (
   <div>
-    <Hero hero="roomsHero" />
+    <Hero hero="roomsHero">
+      <Banner title="our rooms">
+        <Link to="/" className="btn-primary">
+          return home
+        </Link>
+      </Banner>
+    </Hero>
   </div>
 );
 
